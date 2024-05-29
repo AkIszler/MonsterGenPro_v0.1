7c659e2fcfd1798796a96e8bb4cc93efa816a221
Anthony <aiszler29@gmail.com>
2024-05-28 20:44:56 -0700

    first commit

---
+++ MonsterEngine/app.py
@@ -0,0 +1,27 @@
+import random
+
+def generate_monster(id):
+    return {
+        'ID': id,
+        'HP': random.randint(4, 12),
+        'ATK': random.randint(2, 4),
+        'DEF': random.randint(2, 4),
+        'AGI': random.randint(1, 4),
+        'EVA': random.randint(0, 4),
+        'WPN': f'Weapon{id}'  # Just as a placeholder name for the weapon
+    }
+
+def get_random_monster(monsters):
+    return random.choice(monsters)
+
+# Generate a list of 5 monsters
+monsters = [generate_monster(i+1) for i in range(5)]
+
+# Print all monsters
+for monster in monsters:
+    print(monster)
+
+# Get a random monster
+random_monster = get_random_monster(monsters)
+print("\nRandomly selected monster:")
+print(random_monster)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module monsterHunter
+
+go 1.22.2
