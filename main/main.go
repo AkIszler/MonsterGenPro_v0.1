33b62e7594b3a3fe336ada37f707677c96b03da3
Anthony <aiszler29@gmail.com>
2024-05-28 20:58:59 -0700

    update Main.go

--- main/main.go
+++ main/main.go
@@ -1,7 +1,45 @@
-package mai
+package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+)
+
+type Monster struct {
+	ID  int    `json:"ID"`
+	HP  int    `json:"HP"`
+	ATK int    `json:"ATK"`
+	DEF int    `json:"DEF"`
+	AGI int    `json:"AGI"`
+	EVA int    `json:"EVA"`
+	WPN string `json:"WPN"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	scriptPath := "../MonsterEngine/app.py"
+
+	// Command to run the Python script
+	cmd := exec.Command("python3", scriptPath)
+	// Get the output of the Python script
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the raw output for debugging
+	fmt.Println("Raw output:", string(output))
+
+	// Parse the JSON output into a Monster struct
+	var monster Monster
+	err = json.Unmarshal(output, &monster)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
+	// Print the monster struct
+	fmt.Printf("Randomly selected monster: %+v\n", monster)
 }
